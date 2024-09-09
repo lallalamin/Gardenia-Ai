f0412afdcf2da570790c48680be94cb37050cbbf
Mari Hirota <88091545+lallalamin@users.noreply.github.com>
2024-09-08 22:50:47 -0500

    update the styling

--- app/page.js
+++ app/page.js
@@ -1,95 +1,139 @@
+"use client"
 import Image from "next/image";
 import styles from "./page.module.css";
+import NavBar from "@/components/NavBar"
+import { Box, Typography, Stack, Button, TextField, Grid, List, ListItem, ListItemText, IconButton } from "@mui/material";
+import { Delete as DeleteIcon } from '@mui/icons-material';
+import { firestore } from "/firebase";
+import { collection, deleteDoc, doc, getDocs, query, setDoc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  pt: 2,
+  px: 4,
+  pb: 3,
+  gap: 3,
+  flexDirection: 'column',
+};
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [inventory, setinventory] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  const addItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      await setDoc(docRef, { quantity: quantity + 1});
+    } else {
+      await setDoc(docRef, { quantity: 1});
+    }
+    await updateInventory();
+  };
+
+  const removeItem = async (item) => {
+    const docRef = doc(collection(firestore, 'inventory'), item);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const { quantity } = docSnap.data();
+      if (quantity === 1) {
+        await deleteDoc(docRef);
+      } else {
+        await setDoc(docRef, { quantity: quantity - 1 });
+      }
+    }
+    await updateInventory();
+  };
+
+  const updateInventory = async () => {
+    const snapshot = query(collection(firestore, "inventory"));
+    const docs = await getDocs(snapshot);
+    const pantryList = [];
+    docs.forEach((doc) => {
+      pantryList.push({ name: doc.id, quantity: doc.data().quantity});
+    });
+    setinventory(pantryList);
+  };
+
+  const filteredPantry = inventory.filter((item) =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  useEffect(() => {
+    updateInventory();
+  }, []);
+
+  return (
+    <div>
+      <NavBar></NavBar>
+      <Box>
+        <Box width="100vw" height="100vh" display="flex" justifyContent="center" alignItems="center" gap={2} flexDirection="column">
+          <Typography variant="h5"  textAlign="center" fontWeight="bold">
+            Enter your plant baby
+          </Typography>
+          <Box display="flex" justifyContent="center" alignItems="center" gap={2}>
+            <TextField
+              id="outline-basic"
+              label="Add plant"
+              variant="outlined"
+              fullWidth
+              value={itemName}
+              onChange={(e) => setItemName(e.target.value)}
             />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+            
+            <Button variant="contained" onClick={() => { addItem(itemName); setItemName(''); handleClose(); }} sx={{ backgroundColor: '#96D3CC' }}>
+              Add
+            </Button>
+          </Box>
+          <Box >
+            <Box width="800px" height="80px" bgcolor="#96D3CC" display="flex" justifyContent="center" alignItems="center" sx={{borderRadius: "20px"}}>
+              <Typography variant="h6" color="#ffffff" textAlign="center" fontWeight="bold">
+                Your Plant Inventory
+              </Typography>
+            </Box>
+            <Box display="flex" justifyContent="flex-end" mt={2} mb={1}>
+              <TextField
+                label="Search for plant"
+                variant="outlined"
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+                sx={{ marginRight: 2 }} // Adjust margin as needed
+              />
+            </Box>
+            <Stack width="800px" height="300px" spacing={2} overflow="auto">
+              <Grid item xs={12} md={6}>
+                <List dense>
+                  {filteredPantry.map(({ name, quantity }) => (
+                    <ListItem key={name} sx={{ display: 'flex', justifyContent: 'space-between', alignItems: "center", background: "#E5F2E9", marginBottom: "5px", borderRadius: "10px"}}>
+                      <ListItemText
+                        primary={<Typography variant="body1" sx={{ color: '#333' }}>{name.charAt(0).toUpperCase() + name.slice(1)}</Typography>}
+                        secondary={`Quantity: ${quantity}`}
+                      />
+                      <IconButton edge="end" aria-label="delete" onClick={() => removeItem(name)}>
+                        <DeleteIcon />
+                      </IconButton>
+                    </ListItem>
+                  ))}
+                </List>
+              </Grid>
+            </Stack>
+          </Box>
+        </Box>
+      </Box>
     </div>
   );
 }
